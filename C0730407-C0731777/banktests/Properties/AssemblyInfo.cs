1565e70e1f2221f07293e6b3fe3e5acc86eeebce
visualstudio <visualstudio@230-PC06>
2019-03-14 15:24:46 -0400

    3:24pm

---
+++ C0730407-C0731777/banktests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("banktests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("banktests")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("61a2697a-8f36-4871-a102-0a38fcfa1492")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ C0730407-C0731777/banktests/bankaccounttests.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using C0730407_C0731777;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace banktests
+{
+    [TestClass]
+    public class bankaccounttests
+    {
+        [TestMethod]
+        public void Debit_WithValidAmount_UpdatesBalance()
+        {
+            //Arrange
+            double beginningBalance = 11.49;
+            double debitAmount = 4.55;
+            double expected = 7.44;
+            BankAccount account = new BankAccount("Mr. bryan walton", beginningBalance);
+            //Act
+            account.Debit(debitAmount);
+            //Assert
+            double actual = account.Balance;
+            Assert.AreEqual(expected, actual, 0.001, "Account not debited correctly");
+        }
+    }
+}
